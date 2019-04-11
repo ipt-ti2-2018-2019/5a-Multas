1eec3f0171309183248835f3812bed0f5a533eea
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2019-04-11 17:49:41 +0100

    Adição de validadores no modelo do Agente: Required + RegularExpression

--- Multas/Multas/Controllers/AgentesController.cs
+++ Multas/Multas/Controllers/AgentesController.cs
@@ -8,120 +8,110 @@ using System.Web;
 using System.Web.Mvc;
 using Multas.Models;
 
-namespace Multas.Controllers
-{
-    public class AgentesController : Controller
-    {
-        private MultasDB db = new MultasDB();
+namespace Multas.Controllers {
+   public class AgentesController : Controller {
+     
+   // cria VAR que representa a BD
+      private MultasDB db = new MultasDB();
 
-        // GET: Agentes
-        public ActionResult Index()
-        {
-            return View(db.Agentes.ToList());
-        }
+      // GET: Agentes
+      public ActionResult Index() {
 
-        // GET: Agentes/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(agentes);
-        }
+      // procura a totalidade dos Agentes na BD
+      // Instrução feita em LINQ
+      // SELECT * FROM Agentes ORDER BY nome
+         var listaAgentes = db.Agentes.OrderBy(a => a.Nome)   .ToList();
+         
+         return View(listaAgentes);
+      }
 
-        // GET: Agentes/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+      // GET: Agentes/Details/5
+      public ActionResult Details(int? id) {
+         if(id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         // SELECT * FROM Agentes WHERE Id=id
+         Agentes agentes = db.Agentes.Find(id);
+         if(agentes == null) {
+            return HttpNotFound();
+         }
+         return View(agentes);
+      }
 
-        // POST: Agentes/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Agentes.Add(agentes);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+      // GET: Agentes/Create
+      public ActionResult Create() {
+         return View();
+      }
 
-            return View(agentes);
-        }
-
-        // GET: Agentes/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(agentes);
-        }
-
-        // POST: Agentes/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(agentes).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(agentes);
-        }
-
-        // GET: Agentes/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(agentes);
-        }
-
-        // POST: Agentes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Agentes agentes = db.Agentes.Find(id);
-            db.Agentes.Remove(agentes);
+      // POST: Agentes/Create
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Create([Bind(Include = "ID,Nome,Esquadra,Fotografia")] 
+                               Agentes agente) {
+         if(ModelState.IsValid) {
+            db.Agentes.Add(agente);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
+         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+         return View(agente);
+      }
+
+      // GET: Agentes/Edit/5
+      public ActionResult Edit(int? id) {
+         if(id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Agentes agentes = db.Agentes.Find(id);
+         if(agentes == null) {
+            return HttpNotFound();
+         }
+         return View(agentes);
+      }
+
+      // POST: Agentes/Edit/5
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Edit([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes) {
+         if(ModelState.IsValid) {
+            db.Entry(agentes).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         return View(agentes);
+      }
+
+      // GET: Agentes/Delete/5
+      public ActionResult Delete(int? id) {
+         if(id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Agentes agentes = db.Agentes.Find(id);
+         if(agentes == null) {
+            return HttpNotFound();
+         }
+         return View(agentes);
+      }
+
+      // POST: Agentes/Delete/5
+      [HttpPost, ActionName("Delete")]
+      [ValidateAntiForgeryToken]
+      public ActionResult DeleteConfirmed(int id) {
+         Agentes agentes = db.Agentes.Find(id);
+         db.Agentes.Remove(agentes);
+         db.SaveChanges();
+         return RedirectToAction("Index");
+      }
+
+      protected override void Dispose(bool disposing) {
+         if(disposing) {
+            db.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+   }
 }
--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Multas.Models {
    public class Agentes {
 
+
       public int ID { get; set; }
 
+      [Required(ErrorMessage = "Por favor, escreva o Nome do Agente.")]
+      [RegularExpression("([A-ZÁÉÍÓÚa-záéíóúàèìòùäëïöüãõâêîôûçñ]+( |-|')?)+",
+                          ErrorMessage = "Só pode escrever letras no nome. Deve começar por uma maiúscula.")]
       public string Nome { get; set; }
 
+      [Required(ErrorMessage = "Não se esqueça de indicar a Esquadra onde o agente trabalha, por favor.")]
+   //   [RegularExpression("(Tomar|Ourém|Torres Novas|Lisboa|Leiria)")]
       public string Esquadra { get; set; }
 
       public string Fotografia { get; set; }
