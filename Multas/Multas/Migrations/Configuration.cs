8f1becacf644c94ea003c74c6e03f1c2a35a334b
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2019-05-30 15:40:22 +0100

    concretização da fusão dos dois projetos: Sample e Multas.

--- Multas/Multas/Migrations/Configuration.cs
+++ Multas/Multas/Migrations/Configuration.cs
@@ -6,13 +6,13 @@ namespace Multas.Migrations {
    using System.Data.Entity.Migrations;
    using System.Linq;
 
-   internal sealed class Configuration : DbMigrationsConfiguration<MultasDB> {
+   internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext> {
       public Configuration() {
          // é possível fazer automaticamente o RECRIAR da base de dados
          AutomaticMigrationsEnabled = true;
       }
 
-      protected override void Seed(MultasDB context) {
+      protected override void Seed(ApplicationDbContext context) {
          //  This method will be called after migrating to the latest version.
 
          //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
--- Multas/Multas/Models/IdentityModels.cs
+++ Multas/Multas/Models/IdentityModels.cs
@@ -1,40 +1,52 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace IdentitySample.Models
-{
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
+namespace Multas.Models {
+   // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+   public class ApplicationUser : IdentityUser {
+      public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager) {
+         // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+         var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+         // Add custom user claims here
+         return userIdentity;
+      }
+   }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
+   /// <summary>
+   /// representa a criação da Base de Dados da Autenticação
+   /// </summary>
+   public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+      public ApplicationDbContext()
+          : base("MultasConnectionString", throwIfV1Schema: false) {
+      }
 
-        static ApplicationDbContext()
-        {
-            // Set the database intializer which is run once during application start
-            // This seeds the database with admin user credentials and admin role
-            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
-        }
+      static ApplicationDbContext() {
+         // Set the database intializer which is run once during application start
+         // This seeds the database with admin user credentials and admin role
+         Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+      }
 
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+      public static ApplicationDbContext Create() {
+         return new ApplicationDbContext();
+      }
+
+      // vamos colocar, aqui, as instruções relativas às tabelas do 'negócio'
+      // descrever os nomes das tabelas na Base de Dados
+      public virtual DbSet<Multas> Multas { get; set; } // tabela Multas
+      public virtual DbSet<Condutores> Condutores { get; set; } // tabela Condutores
+      public virtual DbSet<Agentes> Agentes { get; set; } // tabela Agentes
+      public virtual DbSet<Viaturas> Viaturas { get; set; } // tabela Viaturas
+
+
+      protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+         modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+         modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+         base.OnModelCreating(modelBuilder);
+      }
+   
+      }
 }
